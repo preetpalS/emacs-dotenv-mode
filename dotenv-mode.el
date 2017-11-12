749e36f2334ec2416f31b38e29d4d61a3cd321e1
Preetpal Sohal <preetpal.sohal@gmail.com>
2017-11-11 23:38:13 -0800

    Updated code (fixes and improvements (better highlighting, lexical-binding)). Added license and necessary metadata for Melpa.

--- dotenv-mode.el
+++ dotenv-mode.el
@@ -1,20 +1,47 @@
+;;; dotenv-mode.el --- Major mode for .lock files -*- lexical-binding: t; -*-
 
-;; Load this file by putting it in a directory that is in your load path
+;; Author: Preetpal S. Sohal
+;; URL: https://github.com/preetpalS/emacs-dotenv-mode
+;; Version: 0.0.1
+;; License: GNU General Public License Version 3
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Usage:
+;; (require 'dotenv-mode) ; unless installed from a package
+
+;;; Code:
 
 (setq dotenv-mode-highlights
-      '(;; ("^[^[:alpha:]_].*\\|:[^[:space:]]+.*\\|=[[:space:]].*" . font-lock-warning-face)
-        ("^[[:alpha:]_]+[[:alpha:][:digit:]_]*" . font-lock-variable-name-face)
-        ;; ("=?\\|:?[[:space:]]" . font-lock-comment-delimiter-face)
-        ("#.*" . font-lock-comment-face)))
+      '(("#.*" . font-lock-comment-face)
+        ("export +" . font-lock-keyword-face)
+        ;; (":\\|=" . font-lock-negation-char-face)
+        ("[[:alpha:]_]+[[:alpha:][:digit:]_]*[:=]+" . font-lock-variable-name-face)
+        ("\$[[:alpha:]]+[[:alpha:][:digit:]_]*" . font-lock-constant-face)
+        ))
 
-(define-derived-mode dotenv-mode
-  prog-mode
+;;;###autoload
+(define-derived-mode dotenv-mode prog-mode ".env"
+  "Major mode for `.env' files."
   (setq-local font-lock-defaults '(dotenv-mode-highlights))
-  (setq-local mode-name "Env"))
+  )
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.env.*\\'" . dotenv-mode))
 
 (provide 'dotenv-mode)
 
-;;;;; Execute the following lines (using your init code after loading this file)
-;;;;; to get basic syntax highlighting on .env files.
-;; (add-to-list 'auto-mode-alist '("\\.env.*\\'" . dotenv-mode))
-;; (require 'dotenv-mode)
+;;; dotenv-mode.el ends here
