77742af1722a63bc6b1e0340b4cf6874c2ee03b9
Preetpal Sohal <preetpal.sohal@gmail.com>
2017-11-19 06:13:12 -0800

    Added short commentary section which summarizes purpose of package. Expanded usage section of commentary.

--- dotenv-mode.el
+++ dotenv-mode.el
@@ -20,9 +20,14 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
+;;
+;; Major mode for editing .env files (which are used for storing
+;; environment variables).
+;;
 ;; Usage:
+;;
 ;; (require 'dotenv-mode) ; unless installed from a package
+;; (add-to-list 'auto-mode-alist '('("\\.env\\..*\\'" . dotenv-mode))) ;; for optionally supporting additional file extensions such as `.env.test' with this major mode
 
 ;;; Code:
 
